99e85ce944706cde6cc3844792ca2bfc26716a61
loickcherimont <loickcherimont@gmail.com>
2025-05-19 08:10:55 +0200

    Establish user authentication : user registration on backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ChatGateway } from './chat.gateway';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { ChatGateway } from './chat.gateway';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+
+// @Module({
+//   imports: [AuthModule],
+//   controllers: [AppController],
+//   providers: [AppService, ChatGateway],
+// })
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://loickcherimont:test123@cluster0.rktks.mongodb.net/chat?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule
+  ]
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from 'src/schemas/user.schema';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('register')
+    async register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
+       return this.authService.register(registerUserDto); 
+    }
+}
